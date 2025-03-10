ec7f4a8efa7bf9a5e572cb5ecd3387fcc7e3cb3a
Mike Radel <etpilot@Mikes-iMac.home>
2025-03-10 10:59:09 -0700

    Add different enemy types with unique behaviors

--- gameEntities.js
+++ gameEntities.js
@@ -38,34 +38,102 @@ class Player {
     }
 }
 
-class CavityMonster {
-    constructor() {
+class Enemy {
+    constructor(type = 'basic') {
+        this.type = type;
         this.reset();
-        this.y = -20; // Start above the screen
-        this.size = 30;
-        this.health = 100;
+        this.setupType();
+    }
+
+    setupType() {
+        switch(this.type) {
+            case 'sugar':
+                this.size = 20;
+                this.health = 50;
+                this.speed = random(4, 6);
+                this.color = color(255, 150, 150);
+                this.points = 150;
+                break;
+            case 'plaque':
+                this.size = 50;
+                this.health = 200;
+                this.speed = random(1, 2);
+                this.color = color(100, 100, 100);
+                this.points = 300;
+                break;
+            default: // basic cavity
+                this.size = 30;
+                this.health = 100;
+                this.speed = random(2, 4);
+                this.color = color(255, 0, 0);
+                this.points = 100;
+        }
     }
 
     reset() {
         this.x = random(20, width - 20);
         this.y = -20;
-        this.speed = random(2, 4);
     }
 
     draw() {
         push();
-        fill(255, 0, 0);
-        circle(this.x, this.y, this.size);
+        fill(this.color);
+        
+        switch(this.type) {
+            case 'sugar':
+                // Draw sugar bug (diamond shape)
+                beginShape();
+                vertex(this.x, this.y - this.size/2);
+                vertex(this.x + this.size/2, this.y);
+                vertex(this.x, this.y + this.size/2);
+                vertex(this.x - this.size/2, this.y);
+                endShape(CLOSE);
+                break;
+            case 'plaque':
+                // Draw plaque boss (irregular blob)
+                beginShape();
+                for(let i = 0; i < 8; i++) {
+                    let angle = TWO_PI * i / 8;
+                    let rad = this.size * (0.8 + random(0.2));
+                    let px = this.x + cos(angle) * rad;
+                    let py = this.y + sin(angle) * rad;
+                    curveVertex(px, py);
+                }
+                endShape(CLOSE);
+                // Draw health bar
+                let healthPct = this.health / 200;
+                fill(255, 0, 0);
+                rect(this.x - this.size/2, this.y - this.size/2 - 10, this.size, 5);
+                fill(0, 255, 0);
+                rect(this.x - this.size/2, this.y - this.size/2 - 10, this.size * healthPct, 5);
+                break;
+            default:
+                // Basic cavity monster (circle)
+                circle(this.x, this.y, this.size);
+        }
         pop();
     }
 
     move() {
-        this.y += this.speed;
-        return this.y > height + this.size; // Return true if monster is off screen
+        switch(this.type) {
+            case 'sugar':
+                // Sugar bugs move in a zigzag pattern
+                this.x += sin(frameCount * 0.1) * 2;
+                this.y += this.speed;
+                break;
+            case 'plaque':
+                // Plaque bosses move slowly but steadily
+                this.y += this.speed;
+                break;
+            default:
+                // Basic enemies move straight down
+                this.y += this.speed;
+        }
+        return this.y > height + this.size; // Return true if enemy is off screen
     }
 
-    hit() {
-        this.health -= 25;
+    hit(damage = 25) {
+        this.health -= damage;
         return this.health <= 0;
     }
 }
@@ -129,55 +197,39 @@ class PowerUp {
 class GameState {
     constructor() {
         this.player = new Player();
-        this.monsters = [];
+        this.enemies = [];
         this.projectiles = [];
         this.powerUps = [];
         this.score = 0;
         this.level = 1;
-        this.monsterSpawnRate = 100;
-        this.powerUpSpawnRate = 500;
         this.frameCount = 0;
+        this.nextSpawnTime = 0;
+        this.spawnInterval = 100;
+        this.bossSpawned = false;
     }
 
     update() {
         this.frameCount++;
         
-        // Spawn monsters
-        if (this.frameCount % this.monsterSpawnRate === 0) {
-            this.monsters.push(new CavityMonster());
-        }
-
-        // Spawn power-ups
-        if (this.frameCount % this.powerUpSpawnRate === 0) {
-            this.powerUps.push(new PowerUp());
+        // Spawn enemies based on level and timing
+        if (this.frameCount >= this.nextSpawnTime) {
+            this.spawnEnemy();
+            this.nextSpawnTime = this.frameCount + this.spawnInterval;
         }
 
         // Update player
         this.player.move();
 
-        // Update monsters
-        for (let i = this.monsters.length - 1; i >= 0; i--) {
-            if (this.monsters[i].move()) {
-                this.monsters.splice(i, 1);
+        // Update enemies
+        for (let i = this.enemies.length - 1; i >= 0; i--) {
+            if (this.enemies[i].move()) {
+                this.enemies.splice(i, 1);
                 this.player.health -= 10;
             }
         }
 
-        // Update projectiles
-        for (let i = this.projectiles.length - 1; i >= 0; i--) {
-            if (this.projectiles[i].move()) {
-                this.projectiles.splice(i, 1);
-            }
-        }
-
-        // Update power-ups
-        for (let i = this.powerUps.length - 1; i >= 0; i--) {
-            if (this.powerUps[i].move()) {
-                this.powerUps.splice(i, 1);
-            }
-        }
-
-        // Check collisions
+        // Update projectiles and check collisions
+        this.updateProjectiles();
         this.checkCollisions();
 
         // Level progression
@@ -186,18 +238,50 @@ class GameState {
         }
     }
 
-    checkCollisions() {
-        // Projectiles hitting monsters
+    spawnEnemy() {
+        let roll = random(100);
+        
+        // Spawn rates change with level
+        if (this.level >= 3 && !this.bossSpawned && this.score > 2000) {
+            // Spawn plaque boss every 2000 points
+            this.enemies.push(new Enemy('plaque'));
+            this.bossSpawned = true;
+        } else if (roll < 20 + this.level * 2) {
+            // Sugar bugs become more common as level increases
+            this.enemies.push(new Enemy('sugar'));
+        } else {
+            // Basic cavity monsters
+            this.enemies.push(new Enemy('basic'));
+        }
+    }
+
+    levelUp() {
+        this.level++;
+        this.spawnInterval = max(30, this.spawnInterval - 10);
+        this.bossSpawned = false; // Allow new boss to spawn
+    }
+
+    updateProjectiles() {
+        // Update projectiles
         for (let i = this.projectiles.length - 1; i >= 0; i--) {
-            for (let j = this.monsters.length - 1; j >= 0; j--) {
+            if (this.projectiles[i].move()) {
+                this.projectiles.splice(i, 1);
+            }
+        }
+    }
+
+    checkCollisions() {
+        // Projectiles hitting enemies
+        for (let i = this.projectiles.length - 1; i >= 0; i--) {
+            for (let j = this.enemies.length - 1; j >= 0; j--) {
                 let p = this.projectiles[i];
-                let m = this.monsters[j];
-                let d = dist(p.x, p.y, m.x, m.y);
+                let e = this.enemies[j];
+                let d = dist(p.x, p.y, e.x, e.y);
                 
-                if (d < (p.size + m.size) / 2) {
-                    if (m.hit()) {
-                        this.monsters.splice(j, 1);
-                        this.score += 100;
+                if (d < (p.size + e.size) / 2) {
+                    if (e.hit()) {
+                        this.enemies.splice(j, 1);
+                        this.score += e.points;
                     }
                     this.projectiles.splice(i, 1);
                     break;
@@ -217,16 +301,10 @@ class GameState {
         }
     }
 
-    levelUp() {
-        this.level++;
-        this.monsterSpawnRate = max(30, this.monsterSpawnRate - 10);
-        // Add level up effects here
-    }
-
     draw() {
         // Draw all game entities
         this.player.draw();
-        this.monsters.forEach(m => m.draw());
+        this.enemies.forEach(e => e.draw());
         this.projectiles.forEach(p => p.draw());
         this.powerUps.forEach(p => p.draw());
 
@@ -245,7 +323,7 @@ class GameState {
         text('Tool: ' + this.player.currentTool, 10, 120);
         
         // Debug info
-        text('Monsters: ' + this.monsters.length, 10, 150);
+        text('Enemies: ' + this.enemies.length, 10, 150);
         text('Projectiles: ' + this.projectiles.length, 10, 180);
         text('Power-ups: ' + this.powerUps.length, 10, 210);
         pop();
